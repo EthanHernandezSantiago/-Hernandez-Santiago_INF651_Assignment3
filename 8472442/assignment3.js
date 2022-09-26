0dfcacf2aa927bf0788f0e48f1997b162f2431f2
EthanHernandezSantiago <112006623+EthanHernandezSantiago@users.noreply.github.com>
2022-09-25 20:41:54 -0500

    Added files

---
+++ 8472442/assignment3.js
@@ -0,0 +1,52 @@
+// Question 1
+
+let myDept, myNumber;
+if (myNumber > 1) {
+    myDept = myNumber;
+}
+
+// Question 2
+
+let myLang, myVaule;
+if (myVaule.length === 10) {
+    myLang = 10;
+} else {
+    myLang = myVaule.length;
+}
+
+// Question 3
+
+if (myVaule.length === 10) {
+    myLang = 10;
+} else if (myVaule.length < 3) {
+    myLang = myVaule.length;
+} else {
+    myLang = myVaule.length;
+}
+
+// Question 4
+
+let myFaveFood;
+if (myNumber === 1) {
+    myFaveFood = 1;
+} else if (myNumber === 2) {
+    myFaveFood = 2;
+} else if (myNumber === 3) {
+    myFaveFood = 3;
+} else if (myNumber === 4) {
+    myFaveFood = 4;
+} else {
+    myFaveFood = myNumber;
+}
+
+// Question 5
+
+let myWeather, myTemp;
+
+if (myTemp > 75) {
+    myWeather = myTemp;
+} else if (myTemp === 75) {
+    myWeather = myTemp;
+} else if (myTemp < 75) {
+    myWeather = myTemp;
+}
